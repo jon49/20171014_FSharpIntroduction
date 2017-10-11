070266a397e6e37cb1e0e92e8a93d6eaaf890ba9
Jon Nyman <jon@selfcare.info>
2017-10-10 17:06:32 -0700

    init

---
+++ fsharp-introduction/fsharp-introduction/json.fsx
@@ -0,0 +1,11 @@
+﻿#I @"..\packages\FSharp.Data.2.3.3\lib\net40"
+#r @"FSharp.Data.dll"
+
+open FSharp.Data
+
+type SimpleJson = JsonProvider<""" {"a": 1, "b": "x", "c": "2017-01-01", "d": 1.23} """>
+
+let simple = SimpleJson.GetSample ()
+
+do
+    printfn "%i - %s - %A - %f" simple.A simple.B simple.C simple.D
---
+++ fsharp-introduction/fsharp-introduction/sql-client.fsx
@@ -0,0 +1,34 @@
+﻿#I @"..\packages\FSharp.Data.SqlClient.1.8.2\lib\net40"
+#r "FSharp.Data.SqlClient.dll"
+
+#I @"..\packages\FSharpx.Async.1.13.2\lib\net45\"
+#r "FSharpx.Async.dll"
+
+open System
+open FSharp.Data
+open FSharpx.Control
+
+[<Literal>]
+let Connection = "Server=JON;Database=AdventureWorks2016CTP3;Trusted_Connection=True;MultipleActiveResultSets=true;"
+
+type DB = SqlProgrammabilityProvider<Connection>
+
+let test (conn : Data.SqlClient.SqlConnection) =
+    use cmd = new DB.HumanResources.sp_GetEmployee_Person_Info_AsOf(conn)
+    cmd.AsyncExecute(DateTime.Now)
+    |> Async.map (fun xs ->
+        xs
+        |> Seq.map (fun x -> sprintf "%s %s" x.FirstName x.LastName)
+        |> String.concat "\n"
+    )
+
+do
+    use conn = new System.Data.SqlClient.SqlConnection(Connection) 
+    conn.Open ()
+    test conn
+    |> Async.RunSynchronously
+    |> printfn "%s"
+
+
+
+
